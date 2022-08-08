5db396b74420040ae9e664152c0231a60db5aa83
mubarak <mubbyre6xx@gmail.com>
2022-08-08 12:48:44 +0100

    design completed

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,7 +5,7 @@ import { contact } from "../../portfolio/portfolio";
 const Contact = () => {
   if (!contact.email) return null;
   return (
-    <section className="section contact center" id="contact">
+    <section className="section contact center reveal" id="contact">
       <h2 className="section__title">Contact</h2>
       <a href={`mailto:${contact.email}`}>
         <span type="button" className="btn btn--outline">
